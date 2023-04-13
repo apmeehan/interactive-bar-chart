2ef4ff48d420b945f8a3570ae6bb0820630ab315
Pete Meehan <apetermeehan@gmail.com>
2023-04-13 14:27:39 +0100

    update example emojis

--- src/InteractiveBarChart.stories.tsx
+++ src/InteractiveBarChart.stories.tsx
@@ -22,9 +22,9 @@ export const BarsAndPictograms = Template.bind({});
 BarsAndPictograms.args = {
   color: 'hsl(330, 90%, 60%)',
   categories: [
-    { label: 'A', pictogram: <ResponsiveEmoji emoji="🎨" /> },
-    { label: 'B', pictogram: <ResponsiveEmoji emoji="✏️" /> },
-    { label: 'C', pictogram: <ResponsiveEmoji emoji="🕺🏻" /> },
+    { label: 'A', pictogram: <ResponsiveEmoji emoji="🌎" /> },
+    { label: 'B', pictogram: <ResponsiveEmoji emoji="🌱" /> },
+    { label: 'C', pictogram: <ResponsiveEmoji emoji="🛰️" /> },
   ],
   initialValues: [2, 4, 3],
 };
@@ -32,9 +32,9 @@ BarsAndPictograms.args = {
 export const PictogramsOnly = Template.bind({});
 PictogramsOnly.args = {
   categories: [
-    { label: 'A', pictogram: <ResponsiveEmoji emoji="🎨" /> },
-    { label: 'B', pictogram: <ResponsiveEmoji emoji="✏️" /> },
-    { label: 'C', pictogram: <ResponsiveEmoji emoji="🕺🏻" /> },
+    { label: 'A', pictogram: <ResponsiveEmoji emoji="🌎" /> },
+    { label: 'B', pictogram: <ResponsiveEmoji emoji="🌱" /> },
+    { label: 'C', pictogram: <ResponsiveEmoji emoji="🛰️" /> },
   ],
   initialValues: [2, 4, 3],
 };
